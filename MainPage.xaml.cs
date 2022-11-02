516d1c5ae1b2f29beb6c860c57e3f1e4f855a2a1
Yojimbo153 <109325053+Yojimbo153@users.noreply.github.com>
2022-11-01 22:10:53 -0700

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using FlowerApp2.Views;
+//using FlowerApp2.MarkupExtensions;
+
+namespace FlowerApp2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        
+        
+        public SetupPage setupPage = new SetupPage();
+        public SetupView2 setupView2 = new SetupView2();
+        //public ImageResourceExtension imageResourceExtension = new ImageResourceExtension();
+       // public EmbeddedImage embeddedImage = new EmbeddedImage();   
+
+        public INavigation Inavigation;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            //imgClip.Source = ImageSource.FromFile("Clip1.png");
+
+            //Routing.RegisterRoute(nameof(setupPage), typeof(SetupPage));
+            //Routing.RegisterRoute(nameof(setupView2), typeof(SetupView2));
+            //INavigationPageController.PushAsync(navigableElement);
+           // Content = new StackPanel();
+        }
+
+        private async void ButtonClicked(object sender, EventArgs e)
+        {
+
+            await Navigation.PushAsync(setupPage);
+
+        }
+
+        private async void ButtonClicked2(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(setupView2);
+
+        }
+
+       /* private async void ButtonClicked3(object sender, EventArgs e)
+        {
+           // await Navigation.PushAsync(imageResourceExtension);
+           // await Navigation.PushAsync(embeddedImage);
+
+        }
+       */
+    }
+
+}
+
